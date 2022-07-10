e90efedb00eb9a18f9423baf241c07892a912a54
saiprashanthvs <77745410+saiprashanthvs@users.noreply.github.com>
2022-07-11 00:12:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const ham=document.getElementById("hamber");
+
+const navbar=document.getElementById('navv');
+
+ham.addEventListener('click',function()
+{
+    navbar.classList.toggle('active')
+});
